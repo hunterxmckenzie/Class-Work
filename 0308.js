950e2b6b4d2c1e22918e25b071a55bd69ab706c0
hunterxmckenzie <98671049+hunterxmckenzie@users.noreply.github.com>
2022-04-05 19:13:21 -0400

    -

---
+++ 0308.js
@@ -0,0 +1,10 @@
+let noun = ["dog", "cat", "elephant"];
+let verb = ["run", "stroll", "gallop"];
+let adjective = ["happily", "sadly", "hastily"];
+
+let noun = nouns [Math.floor(Math.random()*nouns.length)]
+
+let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+
+console.log(sentence) 
+document.write(sentence)
